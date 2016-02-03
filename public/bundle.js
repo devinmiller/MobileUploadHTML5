7d315fbda5d9881cfd44b03e596a1f76181e2ba4
Devin Miller <devin_miller@fsafood.com>
2016-02-02 18:02:00 -0700

    Added upload functionality.

--- public/bundle.js
+++ public/bundle.js
@@ -59,6 +59,109 @@
 	    (0, _jquery2.default)('.file-api').html(Modernizr.filereader);
 	    (0, _jquery2.default)('.filesystem-api').html(Modernizr.filesystem);
 	    (0, _jquery2.default)('.file-input-attr').html(Modernizr.fileinput);
+	
+	    var $inputField = (0, _jquery2.default)('#file');
+	
+	    $inputField.on('change', function (e) {
+	        console.log('derp');
+	
+	        var file = e.target.files[0];
+	
+	        if (file) {
+	            if (/^image\//i.test(file.type)) {
+	                readFile(file);
+	            } else {
+	                alert('Not a valid image!');
+	            }
+	        }
+	    });
+	
+	    function readFile(file) {
+	        var reader = new FileReader();
+	
+	        reader.onloadend = function () {
+	            processFile(reader.result, file.type);
+	        };
+	
+	        reader.onerror = function () {
+	            alert('There was an error reading the file!');
+	        };
+	
+	        reader.readAsDataURL(file);
+	    }
+	
+	    function processFile(dataURL, fileType) {
+	        var maxWidth = 800;
+	        var maxHeight = 800;
+	
+	        var image = new Image();
+	        image.src = dataURL;
+	
+	        image.onload = function () {
+	            var width = image.width;
+	            var height = image.height;
+	            var shouldResize = width > maxWidth || height > maxHeight;
+	
+	            if (!shouldResize) {
+	                sendFile(dataURL);
+	                return;
+	            }
+	
+	            var newWidth;
+	            var newHeight;
+	
+	            if (width > height) {
+	                newHeight = height * (maxWidth / width);
+	                newWidth = maxWidth;
+	            } else {
+	                newWidth = width * (maxHeight / height);
+	                newHeight = maxHeight;
+	            }
+	
+	            var canvas = document.createElement('canvas');
+	
+	            canvas.width = newWidth;
+	            canvas.height = newHeight;
+	
+	            var context = canvas.getContext('2d');
+	
+	            context.drawImage(this, 0, 0, newWidth, newHeight);
+	
+	            dataURL = canvas.toDataURL(fileType);
+	
+	            sendFile(dataURL);
+	        };
+	
+	        image.onerror = function () {
+	            alert('There was an error processing your file!');
+	        };
+	    }
+	
+	    function sendFile(fileData) {
+	        var formData = new FormData();
+	
+	        formData.append('imageData', fileData);
+	
+	        console.log('Sending file...');
+	
+	        _jquery2.default.ajax({
+	            type: 'POST',
+	            url: '/upload',
+	            data: formData,
+	            contentType: false,
+	            processData: false,
+	            success: function success(data) {
+	                if (data.success) {
+	                    alert('Your file was successfully uploaded!');
+	                } else {
+	                    alert('There was an error uploading your file!');
+	                }
+	            },
+	            error: function error(data) {
+	                alert('There was an error uploading your file!');
+	            }
+	        });
+	    }
 	});
 
 /***/ },
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express');
+var multer  = require('multer')
+var upload = multer({ dest: './public/uploads/' })
 
 var app = express();
 var port = process.env.PORT || 1337;
 
 app.use(express.static('public'));
+app.use('/photo', upload);
+
+
+// app.post('/photo', upload.single('photo'), function (req, res, next) {
+//     console.log(req);
+//     // req.file is the `avatar` file
+//     // req.body will hold the text fields, if there were any
+//     res.send({ success: true});
+// })
 
 app.listen(port, () => console.log('Listening on port ' + port));
