a70bb905d9d3d69daf94182d60c5c6aeb98293ec
Devin Miller <devin_miller@fsafood.com>
2016-02-03 15:29:41 -0700

    Uploads now being saved to Azure blob storage.

--- server.js
+++ server.js
@@ -1,27 +1,63 @@
 var express = require('express');
-var multer  = require('multer')
+var busboy  = require('busboy')
 var path = require('path');
+var azure = require('azure-storage');
 
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './public/uploads')
-    },
-    filename: function (req, file, cb) {
-        cb(null, `${Date.now()}-${file.originalname}`)
-    }
-});
-
-var upload = multer({ storage: storage });
+// var storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, './public/uploads')
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, `${Date.now()}-${file.originalname}`)
+//     }
+// });
+// 
+// var upload = multer({ storage: storage });
 
 var app = express();
 var port = process.env.PORT || 1337;
 
 app.use(express.static('public'));
 
-app.post('/upload', upload.single('file'), function (req, res, next) {
+app.get('/upload', function(req,res, next) {
+    var service = azure.createBlobService();
+    
+    service.createContainerIfNotExists('mobile-uploads', { publicAccessLevel : 'blob' }, function(err, result, response) {
+        if(!err) {
+            service.listBlobsSegmented('mobile-uploads', null, function(err, result, response){
+                if(!err){
+                    console.log(result.entries);
+                    // result.entries contains the entries
+                    // If not all blobs were returned, result.continuationToken has the continuation token.
+                    res.send({ success: true, entries: result.entries});
+                }
+            });
+        }
+    });
+});
+
+app.post('/upload', function (req, res, next) {
     // req.file is the `avatar` file
     // req.body will hold the text fields, if there were any
-    res.send({ success: true});
+    var service = azure.createBlobService();
+    
+    service.createContainerIfNotExists('mobile-uploads', { publicAccessLevel : 'blob' }, function(err, result, response) {
+        if(!err) {
+            var fileUpload = new busboy({ headers: req.headers });
+            
+            fileUpload.on('file', function(fieldname, file, filename, encoding, mimetype) {
+                console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+                file.pipe(service.createWriteStreamToBlockBlob('mobile-uploads', `${Date.now()}-${filename}`));
+            });
+            
+            fileUpload.on('finish', function() {
+                //res.writeHead(200, { 'Connection': 'close' });
+                res.send({ success: true});
+            });
+            
+            return req.pipe(fileUpload);
+        }
+    });
 })
 
 app.listen(port, () => console.log('Listening on port ' + port));
